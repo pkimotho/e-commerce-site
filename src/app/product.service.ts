38c630ef206ddb470778e62240b1317ec53a563c
Kimotho <plkimotho@gmail.com>
2019-04-26 21:26:03 +0300

    adds delete path and delete function

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,4 +10,19 @@ export class ProductService {
     return this.db.list('/products').push(product);
   }
 
+  getAll() {
+    return this.db.list('/products');
+  }
+
+  getProduct(productId) {
+    return this.db.object('/products/' + productId);
+  }
+
+  updateProduct(productId, product) {
+    return this.db.object('/products/' + productId).update(product);
+  }
+  deleteProduct(productId) {
+    return this.db.object('/products/' + productId).remove();
+  }
+
 }
