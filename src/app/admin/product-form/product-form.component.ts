c2226ce35676de08fe3e681b0c8870cf82d8b685
Kimotho <plkimotho@gmail.com>
2019-04-26 16:33:55 +0300

    adds a form for products input

---
+++ src/app/admin/product-form/product-form.component.ts
@@ -0,0 +1,26 @@
+import { CategoryService } from './../../category.service';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'app/product.service';
+
+@Component({
+  selector: 'app-product-form',
+  templateUrl: './product-form.component.html',
+  styleUrls: ['./product-form.component.css']
+})
+export class ProductFormComponent implements OnInit {
+
+  categories$;
+
+  constructor(categoryService: CategoryService, private productService: ProductService) {
+    this.categories$ = categoryService.getCategories();
+  }
+
+  ngOnInit() {
+
+  }
+
+  save(product) {
+    this.productService.create(product);
+  }
+
+}
