d58f8a0e1dd0414d5fbde416efda43308ae8c827
Kimotho <plkimotho@gmail.com>
2019-04-26 15:00:08 +0300

    modifies user$ on the template to appUser

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent {
 
-  constructor(public authService: AuthService) { }
+  appUser: AppUser;
+
+  constructor(private authService: AuthService) {
+    authService.appUser$.subscribe(appUser => {
+      this.appUser = appUser;
+    })
+  }
 
   logout() {
     this.authService.logout();
