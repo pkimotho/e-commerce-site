280f7486cb8843907e0ef58a9a309eff58a13f6d
Kimotho <plkimotho@gmail.com>
2019-04-27 10:56:07 +0300

    moves product-card and filter logic to product-card and product-filter components

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,3 +1,6 @@
+import { Product } from './../models/product';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +8,26 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
 
-  constructor() { }
+  products: Product[] = [];
 
-  ngOnInit() {
+  filteredProducts: Product[] = [];
+
+  category: string;
+
+  constructor(productService: ProductService, route: ActivatedRoute) {
+    productService.getAll().subscribe(products => {
+      this.products = products;
+
+      route.queryParamMap.subscribe(params => {
+        this.category = params.get('category');
+
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      })
+    });
   }
 
 }
