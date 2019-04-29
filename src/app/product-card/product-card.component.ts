2a93548037e52cfb82ffcad0eb7c55672d23c8c2
Kimotho <plkimotho@gmail.com>
2019-04-29 11:27:36 +0300

    adds shopping cart display and functionality

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,3 +1,4 @@
+import { ShoppingCartService } from 'app/shopping-cart.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'app/models/product';
 
@@ -10,7 +11,20 @@ export class ProductCardComponent {
 
   @Input('product') product: Product;
   @Input() showActions = true;
+  @Input() shoppingCart;
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
+
+  addToCart() {
+    this.shoppingCartService.addToCart(this.product);
+  }
+  removeFromCart() {
+    this.shoppingCartService.removeFromCart(this.product);
+  }
+  getQuantity() {
+    if (!this.shoppingCart) { return 0 };
+    const item = this.shoppingCart.items[this.product.$key];
+    return item ? item.quantity : 0;
+  }
 
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,22 +1,32 @@
+import { Subscription } from 'rxjs/Subscription';
+import { ShoppingCartService } from 'app/shopping-cart.service';
 import { Product } from './../models/product';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from './../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
 
   products: Product[] = [];
 
   filteredProducts: Product[] = [];
 
+  subscription: Subscription;
+
   category: string;
 
-  constructor(productService: ProductService, route: ActivatedRoute) {
+  cart: any;
+
+  constructor(
+    productService: ProductService,
+    private shoppingCartService: ShoppingCartService,
+    route: ActivatedRoute) {
+
     productService.getAll().subscribe(products => {
       this.products = products;
 
@@ -28,6 +38,14 @@ export class ProductsComponent {
           this.products;
       })
     });
+  }
+  async ngOnInit() {
+    this.subscription = (await this.shoppingCartService.getCart())
+      .subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
