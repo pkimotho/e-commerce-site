9473e1a28e57d730ad64c7d00774321f3b195f4b
Kimotho <plkimotho@gmail.com>
2019-04-29 11:26:17 +0300

    adds total items count badge

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,3 +1,6 @@
+import { ShoppingCart } from './../models/shopping-cart';
+import { Observable } from 'rxjs/Observable';
+import { ShoppingCartService } from 'app/shopping-cart.service';
 import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
@@ -7,18 +10,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
   appUser: AppUser;
 
-  constructor(private authService: AuthService) {
-    authService.appUser$.subscribe(appUser => {
-      this.appUser = appUser;
-    })
-  }
+  shoppingCartItemCount: number;
+
+  cart$: Observable<ShoppingCart>;
+
+  constructor(private authService: AuthService, private shoppingCartService: ShoppingCartService) { }
 
   logout() {
     this.authService.logout();
+  }
+  async ngOnInit() {
+    this.authService.appUser$.subscribe(appUser => {
+      this.appUser = appUser;
+    })
+    this.cart$ = await this.shoppingCartService.getCart();
+
   }
 
 }
