e0a0be25fc129271efd128e244c99b218c2cb13f
Kimotho <plkimotho@gmail.com>
2019-04-29 12:51:40 +0300

    adds get totals accessors

--- src/app/models/shopping-cart-item.ts
+++ src/app/models/shopping-cart-item.ts
@@ -1,5 +1,9 @@
 import { Product } from 'app/models/product';
-export interface ShoppingCartItem {
-    product: Product;
-    quantity: number;
+export class ShoppingCartItem {
+
+    constructor(public product: Product, public quantity: number) { }
+
+    get totalPrice() {
+        return this.product.price * this.quantity;
+    }
 }
--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,12 +1,26 @@
 import { ShoppingCartItem } from './shopping-cart-item';
 export class ShoppingCart {
-    constructor(public items: ShoppingCartItem[]) {
+
+    items: ShoppingCartItem[] = [];
+
+    constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+        for (const productId in this.itemsMap) {
+            const item = itemsMap[productId];
+            this.items.push(new ShoppingCartItem(item.product, item.quantity));
+        }
+    }
+
+    get totalPrice() {
+        let sum = 0;
+        for (let productId in this.items)
+            sum += this.items[productId].totalPrice;
+        return sum;
     }
 
     get totalItemsCount() {
         let count = 0;
-        for (const productId in this.items)
-            count += this.items[productId].quantity;
+        for (const productId in this.itemsMap)
+            count += this.itemsMap[productId].quantity;
 
         return count;
     }
